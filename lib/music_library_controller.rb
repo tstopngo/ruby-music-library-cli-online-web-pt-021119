897d406cb1a6dffb9d79f1ca9bc5da9ede9e59c9
Tracy Ngo <tngo1206@gmail.com>
2019-03-31 03:47:18 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -47,7 +47,7 @@ class MusicLibraryController
     
     if found_artist
       found_artist.songs.each_with_index do |song, index|
-        puts "#{index+1}. #{song.name}"
+        puts "#{index+1}. #{song.name} - #{song.genre}"
     
   end
 
