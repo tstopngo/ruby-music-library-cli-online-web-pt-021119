e7623976deec955b5b22b9d2bb4079ebc53d60ae
Tracy Ngo <tngo1206@gmail.com>
2019-03-31 03:37:50 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -33,5 +33,11 @@ class MusicLibraryController
       puts "#{index+1}. #{artist.name}" 
     end
   end
+  
+  def list_artists
+    Artist.all.sort_by{|artist| artist.name}.each_with_index do |artist, index|
+      puts "#{index+1}. #{artist.name}" 
+    end
+  end
 
 end
